eb414896b814bb7454e04e14ce0887c48b7746d5
stackpilot <stackpilot875@gmail.com>
2026-08-27 10:00:38 +0530

    refactor cart management and integrate remove functionality in checkout

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart,removefromcart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let cartSummeryHTML;
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -114,3 +114,11 @@ document.querySelector('.js-shipping').textContent = shipping === 0 ? 'FREE' : '
 document.querySelector('.js-total-before-tax').textContent = '₹' + totalBeforeTax.toLocaleString('en-IN');
 document.querySelector('.js-tax').textContent = '₹' + tax.toLocaleString('en-IN');
 document.querySelector('.js-order-total').textContent = '₹' + orderTotal.toLocaleString('en-IN');
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click' , () => {
+    const productId = link.dataset.productId;
+    removefromcart(productId),
+    console.log(cart);
+  });
+});
